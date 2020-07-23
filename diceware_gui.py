e2b4e65f5fb1993fe0b22c901885d94ad79999a5
Brian Caulkins <briancaulkins@gmail.com>
2020-07-22 23:13:43 -0500

    playing around with gui elements

--- diceware_gui.py
+++ diceware_gui.py
@@ -7,6 +7,21 @@ class Window(Frame):
         Frame.__init__(self, master)
         self.master = master
 
+        self.pack(fill=BOTH, expand=1)
+
+        text = Label(self, text="Just do it", font=("Helvetica", 18))
+        text.place(x=70,y=90)
+        # create button, link it to clickExitButton()
+        exitButton = Button(self, text="Exit", command=self.clickExitButton)
+
+        # place button at (0,0)
+        exitButton.place(x=0, y=0)
+
+    def clickExitButton(self):
+        exit()
+
+
+
 root = Tk()
 app = Window(root)
 
@@ -16,4 +31,6 @@ app = Window(root)
 root.wm_title("Tkinter window")
 
 # show window
+root.wm_title("DiceWare Generator")
+root.geometry("800x600")
 root.mainloop()
