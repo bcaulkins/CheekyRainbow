83ed0dfb96650c5bf155215202b01dd1c2f9ef38
Brian Caulkins <briancaulkins@gmail.com>
2020-07-22 22:24:03 -0500

    re-order code to create window class first.

--- diceware_gui.py
+++ diceware_gui.py
@@ -2,13 +2,15 @@ from tkinter import *
 import random
 import re
 
-root = Tk()
-app = Window(root)
-
 class Window(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.master = master
+
+root = Tk()
+app = Window(root)
+
+
 
 # set window title
 root.wm_title("Tkinter window")
